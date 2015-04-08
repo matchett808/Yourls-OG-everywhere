f11d963e36300fec5b0e5e41d467c62d6bb797cc
Iain Matchett <iainmatchett@gmail.com>
2015-04-08 16:40:01 +0100

    Initial commit with Skeleton

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Yourls-OG-everywhere
+Plugin URI: https://github.com/matchett808/Yourls-OG-everywhere
+Description: 
+Version: 0.1
+Author: Iain Matchett
+
+*/
+// No direct call
+//if( !defined( 'YOURLS_ABSPATH' ) ) die(); 
+
+
+
